b8e58b971fc2d38232dc678d77ab480b977ea2af
Abril-Oli <abril.olivera2003@gmail.com>
2024-05-30 23:23:25 -0300

    Parcial VBA resuelto y comentado

---
+++ ParcialVba/ParcialVba/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.LabelEmpresa = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxCodigo = New System.Windows.Forms.TextBox()
+        Me.TextBoxColor = New System.Windows.Forms.TextBox()
+        Me.TextBoxLitros = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LabelLtTotalRoja = New System.Windows.Forms.Label()
+        Me.LabelPromLtsAzul = New System.Windows.Forms.Label()
+        Me.ButtonCargar = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(582, 21)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(600, 560)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'LabelEmpresa
+        '
+        Me.LabelEmpresa.AutoSize = True
+        Me.LabelEmpresa.BackColor = System.Drawing.Color.Tan
+        Me.LabelEmpresa.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.0!)
+        Me.LabelEmpresa.Location = New System.Drawing.Point(12, 9)
+        Me.LabelEmpresa.Name = "LabelEmpresa"
+        Me.LabelEmpresa.Size = New System.Drawing.Size(313, 54)
+        Me.LabelEmpresa.TabIndex = 1
+        Me.LabelEmpresa.Text = "Pigmento INC"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBoxLitros)
+        Me.GroupBox1.Controls.Add(Me.TextBoxColor)
+        Me.GroupBox1.Controls.Add(Me.TextBoxCodigo)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 79)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(431, 217)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "INGRESO"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Codigo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Color"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(5, 156)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Litros"
+        '
+        'TextBoxCodigo
+        '
+        Me.TextBoxCodigo.Location = New System.Drawing.Point(0, 56)
+        Me.TextBoxCodigo.Name = "TextBoxCodigo"
+        Me.TextBoxCodigo.Size = New System.Drawing.Size(411, 26)
+        Me.TextBoxCodigo.TabIndex = 3
+        '
+        'TextBoxColor
+        '
+        Me.TextBoxColor.Location = New System.Drawing.Point(0, 116)
+        Me.TextBoxColor.Name = "TextBoxColor"
+        Me.TextBoxColor.Size = New System.Drawing.Size(411, 26)
+        Me.TextBoxColor.TabIndex = 6
+        '
+        'TextBoxLitros
+        '
+        Me.TextBoxLitros.Location = New System.Drawing.Point(0, 189)
+        Me.TextBoxLitros.Name = "TextBoxLitros"
+        Me.TextBoxLitros.Size = New System.Drawing.Size(411, 26)
+        Me.TextBoxLitros.TabIndex = 7
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.LabelPromLtsAzul)
+        Me.GroupBox2.Controls.Add(Me.LabelLtTotalRoja)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 383)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(510, 198)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "ESTADISTICAS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 36)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(179, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Total LTS Pintura Roja"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(5, 126)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(210, 20)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Promedio LTS pintura Azul"
+        '
+        'LabelLtTotalRoja
+        '
+        Me.LabelLtTotalRoja.AutoSize = True
+        Me.LabelLtTotalRoja.Location = New System.Drawing.Point(381, 36)
+        Me.LabelLtTotalRoja.Name = "LabelLtTotalRoja"
+        Me.LabelLtTotalRoja.Size = New System.Drawing.Size(18, 20)
+        Me.LabelLtTotalRoja.TabIndex = 6
+        Me.LabelLtTotalRoja.Text = "0"
+        '
+        'LabelPromLtsAzul
+        '
+        Me.LabelPromLtsAzul.AutoSize = True
+        Me.LabelPromLtsAzul.Location = New System.Drawing.Point(372, 126)
+        Me.LabelPromLtsAzul.Name = "LabelPromLtsAzul"
+        Me.LabelPromLtsAzul.Size = New System.Drawing.Size(18, 20)
+        Me.LabelPromLtsAzul.TabIndex = 7
+        Me.LabelPromLtsAzul.Text = "0"
+        '
+        'ButtonCargar
+        '
+        Me.ButtonCargar.Location = New System.Drawing.Point(145, 302)
+        Me.ButtonCargar.Name = "ButtonCargar"
+        Me.ButtonCargar.Size = New System.Drawing.Size(102, 66)
+        Me.ButtonCargar.TabIndex = 4
+        Me.ButtonCargar.Text = "Cargar"
+        Me.ButtonCargar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1212, 608)
+        Me.Controls.Add(Me.ButtonCargar)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.LabelEmpresa)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents LabelEmpresa As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBoxLitros As TextBox
+    Friend WithEvents TextBoxColor As TextBox
+    Friend WithEvents TextBoxCodigo As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents LabelPromLtsAzul As Label
+    Friend WithEvents LabelLtTotalRoja As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ButtonCargar As Button
+End Class
---
+++ ParcialVba/ParcialVba/Form1.vb
@@ -0,0 +1,181 @@
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+#Region "Dependencias"
+    'Creamos la dependencia de nuestra data table para la empresa de pintura'
+
+    Private dtLatas As DataTable
+
+#End Region
+
+#Region "Inicializacion"
+    'Damos inicio a nuestra nueva data table, creamos y nombramos sus columnas'
+    Private Sub InicializarDataTable()
+        Dim dt As New DataTable
+        dt.Columns.Add("Codigo")
+        dt.Columns.Add("Color")
+        dt.Columns.Add("Litros")
+
+        Me.SetdtLatas(dt)
+
+
+    End Sub
+
+#End Region
+
+#Region "Acciones"
+    'Creamos nuestra funcion SetdtLatas declarabdola nuestra datatable'
+    Private Sub SetdtLatas(dt As DataTable)
+        Me.dtLatas = dt
+
+        'Conectamos nuestra datatable a la grilla de la ventana grafica mediante un Datasource'
+        DataGridView1.DataSource = dt
+    End Sub
+
+
+    'Creamos una funcion -RecargarEstadisticas- para guardar nuestras variables y logica'
+    Private Sub RecargarEstadisticas()
+
+        'Declaramos nuestras variables a utilizar'
+        Dim acumPintura
+        Dim acumPinturaRoja As Integer = 0
+        Dim contPinturaRoja = 0
+        Dim acumPinturaAzul As Integer = 0
+        Dim contPinturaAzul As Integer = 0
+
+
+        'Recorremos las filas con un for each,  para tomar los datos ingresados hacia las variables '
+        For Each renglon As DataRow In dtLatas.Rows
+            acumPintura = renglon("Litros")
+
+            'Declaramos el ingreso de dato tipo String para lo que se ingrese en el el reglon Color'
+            Dim color As String = renglon("Color")
+
+            'Hacemos un condicional cuandoel color ingresado sea rojo, se guarden sus litros en un acumulador de pintura roja'
+            If color.ToUpper() = "ROJO" Then
+                acumPinturaRoja += renglon("Litros")
+
+            End If
+
+            'Condicional de filtrado, guardado y contador'  
+            If color.ToUpper() = "AZUL" Then
+
+                'cuando el color ingresado sea azul, se guardan sus litros en un acumulador de pintura Azul'
+                acumPinturaAzul += renglon("Litros")
+
+                'una vez guardada su cantidad en el acumulador, sumamos 1 al contador'
+                contPinturaAzul += 1
+            End If
+
+            'Condicional para evitar errores de division por 0'
+            'Si tenemos mas de 0 pinturas azules, podremos calcular un promedio'
+            If contPinturaAzul > 0 Then
+
+                'calculamos el promedio de pintura azul, con nuestros LTS acumulados divididos por la cantidad de pinturas que tenemos'
+                Dim promedioAzul = acumPinturaAzul / contPinturaAzul
+
+                'Muestreo en ventana grafica, asignamos nuestra var de promedio junto a un toString'
+                LabelPromLtsAzul.Text = promedioAzul.ToString("#0.00")
+            Else
+                'Si no tenemos un promedio, muestra cantidad nula=0'
+                LabelPromLtsAzul.Text = "#0.00"
+            End If
+
+        Next
+
+
+        'muestreo de LTS totales de pintura roja, junto a un toString'
+        LabelLtTotalRoja.Text = acumPinturaRoja.ToString("#0.00")
+
+
+
+
+    End Sub
+    'Funcion de limpieza de textboxes'
+    'Esta funcion vacia textboxes despues de que se agreguen datos a la gridview'
+    Private Sub LimpiarTextbox()
+        TextBoxCodigo.Text = ""
+        TextBoxColor.Text = ""
+        TextBoxLitros.Text = ""
+
+    End Sub
+
+
+#End Region
+
+
+#Region "Eventos"
+    ''
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InicializarDataTable()
+
+    End Sub
+
+
+    'FUNCIONALIDAD DEL BOTON AGREGAR'
+    Private Sub ButtonCargar_Click(sender As Object, e As EventArgs) Handles ButtonCargar.Click
+
+
+        '⭣ VALIDACIONES con try catch⭣ '
+
+
+        Try
+
+            'VALIDACION INGRESO SOLO DE DATOS NUMERICOS  '
+
+            'Declaramos el tipo de dato-double,integer,string- que se debe ingresar en nuestros TextBox'
+            Dim codigo As Integer = Integer.Parse(TextBoxCodigo.Text)
+            Dim color As String = TextBoxColor.Text
+            Dim Litros As Double = Double.Parse(TextBoxLitros.Text)
+
+
+            'VALIDACION TEXTBOX VACIO'
+            'Validamos que si el TextBox de color queda vacio, nos muestre un mensaje de error'
+
+            If color = "" Then
+                MessageBox.Show("Por favor, completar campo Color vacio.", "Error de ingreso", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                Return
+            End If
+
+            'VALIDACION CODIGO UNICO'
+            'validamos recorriendo con un for each que el codigo actual no este repetido, sino nos muestra un msj de error'
+
+            For Each renglon As DataRow In Me.dtLatas.Rows
+                If renglon("Codigo") = codigo Then
+                    MessageBox.Show("Codigo repetido, intente otro codigo", "ERROR DE REPETICION", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+                    Return
+                End If
+            Next
+
+            'VALIDACION: NUMEROS NEGATIVOS tanto para codigo como para Lts'
+            'validamos que si los numeros ingresados codigo y tmb litros son  < 0(negativos), nos muestre un msj error pidiendo ingreso de num positivos'
+
+            If codigo < 0 AndAlso Litros < 0 Then
+                MessageBox.Show("Error numerico", "ingrese valores positivos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            'Agregamos nuestros parametros a nuestra dataTable'
+            Me.dtLatas.Rows.Add(codigo, color, Litros)
+
+            'En caso de no cumplir alguna de estas validaciones, nos muestra un mensaje de error de campos'
+        Catch ex As Exception
+            MessageBox.Show("Verificar nuevamente los datos", "Error de campos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+        'Volvemos a llamar a nuestras funciones, para limpiar los textbox y recargar de nuevo nuestras funciones del programa'
+
+        Me.LimpiarTextbox()
+        RecargarEstadisticas()
+    End Sub
+
+
+
+
+#End Region
+End Class
